0fbbbaac27ff5416dd418bc5fc6d4544420f7d8b
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-01-16 17:59:09 +0100

    ADD- ROUTES, HEADER COMPONENT + ROUTERS VIEWS

---
+++ vite-boolfolio/src/router.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import ProjectIndex from './pages/projects/Index.vue'
+import ProjectShow from './pages/projects/Show.vue'
+import Contact from './pages/Contact.vue'
+import NotFound from './pages/404.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: ProjectIndex,
+		},
+		{
+			path: '/blog/:slug',
+			name: 'project.show',
+			props: true,
+			component: ProjectShow,
+		},
+		{
+			path: '/contatti',
+			name: 'contacts',
+			component: Contact,
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'not-found',
+			component: NotFound,
+		},
+	],
+})
+
+export { router }
